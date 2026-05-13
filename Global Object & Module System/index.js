7d9163d10b49c3dd6c11838070c30affb18276ff
shadid06 <hasnainbauet@gmail.com>
2026-05-13 12:52:29 +0600

    global & module system

---
+++ Global Object & Module System/index.js
@@ -0,0 +1,11 @@
+// setTimeout(()=>{
+// console.log('hello world');
+// },1000);
+//youtube: https://www.youtube.com/watch?v=IFBeIUX3J8M
+import { people, a, print } from "./people.js";
+import lodash from 'lodash';
+// const a=5;
+console.log(a);
+console.log(people);
+print();
+console.log(lodash.last(people));
---
+++ Global Object & Module System/people.ts
@@ -0,0 +1,10 @@
+const people=['asik','yelle','jaman'];
+
+const a=7;
+
+function print(){
+    console.log(people);
+    console.log(a);
+}
+
+export  {people,print, a};
