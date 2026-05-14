4a0949953a818068056b914bfc2c67869d4971b0
shadid06 <hasnainbauet@gmail.com>
2026-05-14 16:39:25 +0600

    stream and buffer

--- Server & Core Modules/http.js
+++ Server & Core Modules/http.js
@@ -1,8 +1,17 @@
 import http from 'node:http';
 const server = http.createServer((req, res) => {
-    if (req.url === '/')
+    if (req.url === '/') {
         res.write('hello world');
-    res.end();
+        res.end();
+    }
+    else if (req.url === '/about') {
+        res.write('this is about page');
+        res.end();
+    }
+    else {
+        res.write('404 not found');
+        res.end();
+    }
 });
 // server.on('connection',()=>{
 //     console.log('new connection');
